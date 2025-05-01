b5cd639e6e33fde359973fbadd5cf9dbd73abb22
DanieleMorotti <daniele.morotti.99@gmail.com>
2025-05-01 15:40:55 +0200

    Removed splashscreens from sw

--- projects/SegnapuntiGenerico/sw.js
+++ projects/SegnapuntiGenerico/sw.js
@@ -1,18 +1,12 @@
 // if you change some static files you change also these name and the cache will be updated
-let staticCache = 'segnapunti_cache-v3.5.1';
-let dynamicCache = 'segnapunti_cache_d-v3.5.1';
+let staticCache = 'segnapunti_cache-v3.5.2';
+let dynamicCache = 'segnapunti_cache_d-v3.5.2';
 let staticAssets = [
     './','./index.html','./img/quad-appunti.png','./img/quadAppunti192.png','./img/quadAppunti512.png', './img/favicon.ico',
     './img/maskable_icon.png','./img/ios_share.png','./libraries/jquery-3.5.1.min.js','./main.js','./style.css',
     'https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css','https://use.fontawesome.com/releases/v5.6.1/css/all.css',
     'https://cdn.jsdelivr.net/npm/shepherd.js@8.3.1/dist/css/shepherd.css','https://unpkg.com/vue@3/dist/vue.global.prod.js', 
-    'https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js','https://cdn.jsdelivr.net/npm/shepherd.js@8.3.1/dist/js/shepherd.min.js',
-    './img/splashscreens/apple-splash-ipad-pro-12-9.png', './img/splashscreens/apple-splash-ipad-pro-l-12-9.png', './img/splashscreens/apple-splash-ipad-pro-11.png',
-    './img/splashscreens/apple-splash-ipad-pro-l-11.png', './img/splashscreens/apple-splash-ipad-mini.png', './img/splashscreens/apple-splash-ipad-mini-l.png',
-    './img/splashscreens/apple-splash-ipad-air.png', './img/splashscreens/apple-splash-ipad-air-l.png', './img/splashscreens/apple-splash-ipad.png',
-    './img/splashscreens/apple-splash-ipad-l.png', './img/splashscreens/apple-splash-iph-12-pro-max.png', './img/splashscreens/apple-splash-iph-12-pro.png',
-    './img/splashscreens/apple-splash-iph-12-mini.png', './img/splashscreens/apple-splash-iph-11-pro-max.png', './img/splashscreens/apple-splash-iph-11.png',
-    './img/splashscreens/apple-splash-iph-8-plus.png', './img/splashscreens/apple-splash-iph-7.png'
+    'https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js','https://cdn.jsdelivr.net/npm/shepherd.js@8.3.1/dist/js/shepherd.min.js'
 ];
 
 // On install - caching the application static files
